55c9294e60d6f8c406618b4be35de40774616aa5
Juan Antonio Campoy Lira <juan.campoy@KOSMOS.local>
2021-05-14 13:04:09 -0500

    Fisrt commit

---
+++ src/pages/BadgeNew.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import './styles/BadgeNew.css';
+import header from '../images/badge-header.svg';
+import Badge from '../components/Badge';
+import BadgeForm from '../components/BadgeForm';
+import Navbar from '../components/Navbar';
+
+class BadgeNew extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="Logo" />
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <Badge
+                firstName="Juan Antonio"
+                lastName="Campoy Lira"
+                twitter="Campoy"
+                jobTitle="Full Stack Engineer"
+                avatarUrl="https://s.gravatar.com/avatar/5be7e6a29ce321fc07b7caaafedd16f9?s=80"
+              />
+            </div>
+
+            <div className="col-6">
+              <BadgeForm />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BadgeNew;
